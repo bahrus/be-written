913794495bbfe5e0ad42ddf88634c27449218a1e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-28 09:17:56 -0400

    0.0.52

--- be-written.js
+++ be-written.js
@@ -1,9 +1,8 @@
 // @ts-check
 import { config as beCnfg } from 'be-enhanced/config.js';
 import { BE } from 'be-enhanced/BE.js';
-/** @import {BEConfig} from './node_modules/be-enhanced/types.d.ts' */
+/** @import {BEConfig, IEnhancement,  BEAllProps} from './node_modules/be-enhanced/types.d.ts' */
 /** @import {Actions, PAP, AllProps, AP} from './types.d.ts' */;
-/** @import {IEnhancement,  BEAllProps} from './node_modules/trans-render/be/types.d.ts' */;
 
 /**
  * @implements {Actions}
